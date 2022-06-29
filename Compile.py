b772fb3c993aacb030eb1476ab3504770a58f677
mluczynskii <mateusz.luczynski02@gmail.com>
2022-06-29 19:32:31 +0200

    Fix compiling script

--- Compile.py
+++ Compile.py
@@ -3,8 +3,9 @@ import os
 main = "javac -d ./bin ./main/Window.java"
 entity = "javac -d ./bin ./entity/*.java"
 item = "javac -d ./bin ./items/*.java"
-pickup = "javac -d ./bin ./items/*.java"
+pickup = "javac -d ./bin ./pickup/*.java"
 
 os.system(main)
 os.system(entity)
 os.system(item)
+os.system(pickup)
